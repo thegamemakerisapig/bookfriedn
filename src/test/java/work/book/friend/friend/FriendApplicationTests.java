fc16bf27648e7d10a9cfc7407d742ddc92ce20ef
gamemaker <417824246@qq.com>
2020-02-21 18:24:06 +0800

    创建项目

---
+++ src/test/java/work/book/friend/friend/FriendApplicationTests.java
@@ -0,0 +1,13 @@
+package work.book.friend.friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
